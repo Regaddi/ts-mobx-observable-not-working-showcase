4dd0d479ba51cc9855d2328c46d85ea2e4ae900d
Christian Stuff <cstuff@lyska.io>
2019-01-23 12:17:47 +0100

    init

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { observer } from "mobx-react";
+import { observable } from "mobx";
+
+@observer
+class App extends React.Component<any> {
+  @observable count: number = 0;
+  render() {
+    return (
+      <div>
+        <div>{this.count}</div>
+        <button onClick={this.increment}>+</button>
+      </div>
+    );
+  }
+  increment = () => {
+    console.log(this.count);
+    this.count++;
+  };
+}
+
+render(<App />, document.getElementById("root"));
